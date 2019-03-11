d91c44d88ad3221125941eb08f860d49dac858eb
Daniel Antonio Villaseñor Alvarez <dava@MacBook-Pro-de-Daniel.local>
2019-03-10 23:03:24 -0600

    Se añade funcion metalPath y se agrega el DAG

--- airflow/airflowMetal.py
+++ airflow/airflowMetal.py
@@ -1,5 +1,7 @@
+import os
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 
 default_args = {
@@ -12,8 +14,22 @@ default_args = {
 
 dag = DAG('metalDAG', default_args=default_args, schedule_interval=None)
 
+def metalPath():
+    for root, dirs, files in os.walk("/Users/dava/Documentos/Python/ScrapyMetal"):
+        for file in files:
+            if file.endswith(".csv"):
+                path = os.path.join(root, file)
+    return path
+
 scrapyMetal = BashOperator(
     task_id='scrapyMetalTask',
     bash_command='/scrapyMetal.sh',
     dag=dag)
 
+metalPath = PythonOperator(
+    task_id='metalPathTask',
+    python_callable=metalPath,
+    dag=dag
+)
+
+scrapyMetal>>metalPath
