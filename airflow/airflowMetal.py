4f4d65ca31dba8c4d5632a4cde13e2273de3da4c
Daniel Antonio Villaseñor Alvarez <dava@MacBook-Pro-de-Daniel.local>
2019-03-10 22:20:45 -0600

    Se añade airflow y se genera el primer DAG

---
+++ airflow/airflowMetal.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'dava',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 3, 10),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG('metalDAG', default_args=default_args, schedule_interval=None)
+
+scrapyMetal = BashOperator(
+    task_id='scrapyMetalTask',
+    bash_command='/scrapyMetal.sh',
+    dag=dag)
+
---
+++ airflow/scrapyMetal.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /Users/dava/Documentos/Python/ScrapyMetal/metalscrapy/metalscrapy/spiders
+echo "Ejecutando spider !! ;)"
+scrapy crawl wikiMetal
+echo "Crawling completado :)))) !!"
